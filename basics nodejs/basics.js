075af0ed107cda8f50c93e67a2844cb774e1f198
KitsuneNoctus <theceintiant@gmail.com>
2024-11-04 13:57:34 -0800

    Learning about variables

--- basics nodejs/basics.js
+++ basics nodejs/basics.js
@@ -2,3 +2,21 @@ console.log("Hello World!");
 console.log("444");
 console.log(444);
 console.log(2 + 4 * 6 - 5 /3);
+
+var firstName = "Hank";
+let lastName = "Shaw";
+const age = 21;
+const superImportantValue = "secret"
+let boolTrue = true;
+let boolFalse = false;
+
+
+firstName = "Mark";
+lastName = "Beattle";
+// superImportantValue = "not secret anymore"
+
+console.log(firstName);
+console.log(lastName);
+console.log(superImportantValue);
+console.log(boolTrue);
+console.log(boolFalse);
