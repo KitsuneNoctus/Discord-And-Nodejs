79274b53d5093fa3e8e401596ed51239dea6c4d1
KitsuneNoctus <theceintiant@gmail.com>
2024-11-06 14:04:56 -0800

    Converting milliseconds to date and time

---
+++ basics nodejs/appTime.js
@@ -0,0 +1,12 @@
+// convert milliseconds to date and time
+
+
+let date = new Date(0);
+let date2 = new Date(36000000);
+
+console.log(date.toString());
+console.log(date2.toString());
+
+let date3 = new Date(34560000000);
+
+console.log(date3.toString());
